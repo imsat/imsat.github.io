3a2368f7bad7cfc53d20f32b3d6f622101a922fe
imsat <satyajit.cse@yahoo.com>
2025-05-09 19:25:02 +0600

    update portfolio

--- work/gistbox/js/components/Gist.js
+++
@@ -1,9 +0,0 @@
-var Gist = React.createClass({
-    render: function() {
-        return (
-            <div>
-            {this.props.username}'s last Gist is <a href={this.props.url} target="_blank">Here</a> 
-        </div>
-        );
-    }
-});
